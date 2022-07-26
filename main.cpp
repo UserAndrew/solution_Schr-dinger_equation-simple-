1c7a1ec3c5f166f45e61662227755e2a5b0f9b5f
UserAndrew <41081599+UserAndrew@users.noreply.github.com>
2022-07-26 21:17:49 +0300

    Add files via upload
    fixed

--- main.cpp
+++ main.cpp
@@ -16,15 +16,13 @@ double Psi_solution(double x)
 
 int main()
 {
-    const int N = 512;
-    const int M = 55;
+    const int N = 2048;
+    const int M = 10000;
     const double Xmin = -15.;
     const double Xmax = 15.;
     const double dt = 0.01;
-    const double dx = (Xmax - Xmin)/(N-1);
-    const double nuNyq = 1/(2*dx);
-    //const double dp = 2.0*M_PI/(dx*N);
-    const double dp = (N - 1)/(N*(Xmax - Xmin));
+    const double dx = (Xmax - Xmin)/N;
+    const double dp = 2.0*M_PI/(dx*N);
     std::vector<double> coordinate(N);
     fftw_complex *func_in = new fftw_complex[N];
     fftw_complex *func_out = new fftw_complex[N];
@@ -38,38 +36,17 @@ int main()
         X = Xmin+dx*i;
         coordinate[i] = X;
         func_in[i][0] = exp(-X*X);
-        /*double X = 0.0;
-        if(i < N/2)
-        {
-            X = dx*i;
-            coordinate[N/2+i] = X;
-            func_in[N/2+i][0] = exp(-X*X);
-        }
-        else
-        {
-            X = dx*i-dx*N;
-            coordinate[i-N/2] = X;
-            func_in[i-N/2][0] = exp(-X*X);
-        }*/
-        //double X = dx*i;
-        //func_in[i][0] = exp(-X*X);
         func_in[i][1] = 0.;
     }
 
-    std::cout<<func_in[1][0]<<' '<<func_in[N/2][0]<<' '<<func_in[N][0]<<std::endl;
-
     double *p = new double[N];
-    /*for(int i = 0; i < N/2; ++i)
+    for(int i = 0; i < N/2; ++i)
     {
-        p[i] = dp*(double)i;
+        p[i] = dp*i;
     }
-    for(int i = N/2; i < N; i++)
+    for(int i = N/2; i < N; ++i)
     {
         p[i] = -dp*(N-i);
-    }*/
-    for(int i = 0; i < N; ++i)
-    {
-        p[i] = -nuNyq + dp*N;
     }
 
     for (int i = 0; i < M; ++i)
@@ -78,15 +55,9 @@ int main()
         {
             func_in[j][0] = exp(-V(coordinate[j])*dt)*func_in[j][0];
         }
+
         fftw_execute(plan_fwd);
-        for (int j = 0; j < N; j++)
-        {
-            double re = func_out[j][0];
-            double im = func_out[j][1];
-            double phase = - double(j)*M_PI;
-            func_out[j][0] = re*cos(phase) - im*sin(phase);
-            func_out[j][1] = re*sin(phase) + im*cos(phase);
-        }
+
         for (int j = 0; j < N; ++j)
         {
             func_out[j][0] = exp(-p[j]*p[j]*dt/2.)*func_out[j][0];
@@ -98,23 +69,25 @@ int main()
             func_in[j][0] = (1./N)*func_in[j][0];
         }
 
-        double norma = 0;
+        double re_norma = 0;
         for (int j = 0; j < N; ++j)
         {
-            norma = norma + (func_in[j][0]*func_in[j][0])*dx;
+            re_norma = re_norma + (func_in[j][0]*func_in[j][0])*dx;
         }
 
 
-        double member = 1./sqrt(norma);
+        double re_member = 1./sqrt(re_norma);
 
         for (int j = 0; j < N; ++j)
         {
-            func_in[j][0] = func_in[j][0]*member;
+            func_in[j][0] = func_in[j][0]*re_member;
+        }
+
+        for (int j = 0; j < N; ++j)
+        {
+            func_in[j][1] = 0;
         }
     }
-
-    std::cout<<func_out[1][0]<<' '<<func_out[N/2][0]<<' '<<func_out[N][0]<<std::endl;
-    std::cout<<func_in[1][0]<<' '<<func_in[N/2][0]<<' '<<func_in[N][0]<<std::endl;
 
     std::ofstream _out("Analitical_solution.dat");
     _out.precision(4);
